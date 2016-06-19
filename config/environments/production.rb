fec703b4992fd7d1658bf4a5629290bef7a1fb94
adibamine <adib.amine.mdr@gmail.com>
2016-06-19 18:14:02 +0000

    smtp config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://nameless-wildwood-94934.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'radiant-wildwood-84960.herokuapp.com',
+    user_name:            'adib.elaraki',
+    password:             'greenboys05@@',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
